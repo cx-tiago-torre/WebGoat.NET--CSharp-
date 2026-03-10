18cb769227a63f709b6e909a6a5ceacfa78d4d9b
cx-tiago-torre <tiago.torre@checkmarx.com>
2026-03-10 13:19:19 +0100

    spaces

--- WebGoat/App_Code/DB/DbConstants.cs
+++ WebGoat/App_Code/DB/DbConstants.cs
@@ -5,7 +5,7 @@ namespace OWASP.WebGoat.NET.App_Code.DB
 {
     public class DbConstants
     {
-        //Keys 
+        // Keys
         public const string KEY_DB_TYPE = "dbtype";
         public const string KEY_CLIENT_EXEC = "client";
         public const string KEY_HOST = "host";
@@ -15,7 +15,7 @@ namespace OWASP.WebGoat.NET.App_Code.DB
         public const string KEY_UID = "uid";
         public const string KEY_PWD = "pwd";    
         
-        //DB Types
+        // DB Types
         public const string DB_TYPE_MYSQL = "MySql";
         public const string DB_TYPE_SQLITE = "Sqlite";
         public const string CONFIG_EXT = "config";
